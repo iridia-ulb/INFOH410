6e2ef02e3f6a1cfbe1bd01541c7de9c055297b2c
Ken <ken@kenh.fr>
2021-03-31 15:05:22 +0200

    refactor filenames

--- TP_NN/src/ex2_template.py
+++ TP_NN/src/ex2_template.py
@@ -12,14 +12,19 @@ def main():
     POINTS = 1000
     DIMENSION = 2
     CLASSES = 3
+    # create a 4 layer NN with 10 neurons per hidden layer
     NN_SHAPE = [DIMENSION, 10, 10, 10, CLASSES]
 
     x, y = create_dataset(N=POINTS, K=CLASSES, D=DIMENSION)
     x = np.expand_dims(x, -1)
+    # transform data to categorical
     y = tf.keras.utils.to_categorical(y, num_classes=CLASSES)
     y = np.expand_dims(y, -1)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True)
+    # automatically split dataset in train/test with 20% as test
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=42, shuffle=True
+    )
 
     nn = NeuralNetwork(NN_SHAPE)
     pbar = tqdm(range(EPOCH))
@@ -27,15 +32,21 @@ def main():
         shuffle(x_train, y_train)
         for _x, _y in zip(x_train, y_train):
             nn.train(_x, _y)
+
+        # compute train and validation accuracy
         train_accurracy = nn.evaluate(x_train, y_train)
-        val_accuracy = nn.evaluate(x_test, y_test)
-        pbar.set_description(f"Epoch {e:03}/{EPOCH} - Train {train_accurracy:.3f}% - Test {val_accuracy:.3f}% ")
+        test_accuracy = nn.evaluate(x_test, y_test)
+        pbar.set_description(
+            f"Epoch {e:03}/{EPOCH} - Train {train_accurracy:.3f}% - Test {test_accuracy:.3f}% "
+        )
+
+    # compute train and validation accuracy
     train_accurracy = nn.evaluate(x_train, y_train)
-    val_accuracy = nn.evaluate(x_test, y_test)
-    print(f"Train {train_accurracy:.3f}% - Test {val_accuracy}%")
+    test_accuracy = nn.evaluate(x_test, y_test)
+    print(f"Train {train_accurracy:.3f}% - Test {test_accuracy}%")
 
     plot_contour(nn, x, y)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
